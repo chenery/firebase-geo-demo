d38f195d16269f302b8394cda15fb2f0875a2b39
paulchenery <paul@solidstategroup.com>
2017-04-22 19:23:40 +0100

    add auth and add/remove location

--- js/location.js
+++ js/location.js
@@ -1,4 +1,13 @@
+/**
+ * todo consider how users are adding to the location index, and more importantly, how they are removed
+ * (i.e. upon disconnect?).
+ */
 (function () {
+
+    // GLOBALS for function
+    var _loggedInUserFirebaseRef = null;
+    var _loggedInUserLocationPin = null;
+
     // Initialize the Firebase SDK
     var config = {
         apiKey: "AIzaSyBiUQMPLNgTH81FNMWFuza32CONngyFhAY",
@@ -17,11 +26,102 @@
     // Create a new GeoFire instance at the random Firebase location
     var geoFire = new GeoFire(firebaseLocationsRef);
 
+    var _loginBtn = $('#loginbtn');
+    var _loginDiv = $('#login');
+    _loginBtn.click(triggerLogin);
+
+    var _activateDiv = $('#activate');
+    $('#activateBtn').click(registerLocation);
+
+    var _deActivateDiv = $('#deActivate');
+    $('#deActivateBtn').click(deRegisterLocation);
+
+    var _queryDiv = $('#query');
+
     $('#searchButton').click(query);
     $('#createUsers').click(addData);
 
     var logRef = $('#log');
 
+    // check for logged in user
+    firebase.auth().getRedirectResult().then(function(result) {
+
+        handleLogin(result);
+
+    }).catch(function(error) {
+        // Handle Errors here.
+        var errorCode = error.code;
+        var errorMessage = error.message;
+        // The email of the user's account used.
+        var email = error.email;
+        // The firebase.auth.AuthCredential type that was used.
+        var credential = error.credential;
+        // ...
+        log("*** Error logging in: " + errorMessage + " ***");
+    });
+
+    function triggerLogin() {
+
+        var provider = new firebase.auth.FacebookAuthProvider();
+
+        provider.addScope('public_profile');
+        provider.addScope('user_friends');
+        provider.addScope('email');
+
+        firebase.auth().signInWithRedirect(provider);
+    }
+
+    function handleLogin(result) {
+        if (result.credential) {
+            // This gives you a Facebook Access Token. You can use it to access the Facebook API.
+            var token = result.credential.accessToken;
+            // ...
+        }
+        // The signed-in user info.
+        var user = result.user;
+        if (user) {
+            log("*** Logged in as user: " + user.displayName + " ***");
+            _loggedInUserFirebaseRef = saveUser(user.email, user.displayName, user.photoURL);
+            _loginDiv.hide();
+            _activateDiv.show();
+        } else {
+            log("*** Not logged in ***");
+            _loginDiv.show();
+        }
+    }
+
+    function registerLocation() {
+        if (navigator.geolocation) {
+            navigator.geolocation.getCurrentPosition(function(position) {
+                log("User is located at lat: " + position.coords.latitude + " lng: " + position.coords.longitude);
+
+                var locationPin = [position.coords.latitude, position.coords.longitude];
+                _loggedInUserLocationPin = locationPin;
+
+                if (_loggedInUserFirebaseRef) {
+                    geoFire.set(_loggedInUserFirebaseRef.key, locationPin).then(function () {
+                        log("user location for user: " + _loggedInUserFirebaseRef.key + " set");
+                        _activateDiv.hide();
+                        _deActivateDiv.show();
+                        _queryDiv.show();
+                    });
+                }
+            });
+        } else {
+            log("Geolocation is not supported by this browser.");
+        }
+    }
+
+    function deRegisterLocation() {
+        if (_loggedInUserFirebaseRef) {
+            geoFire.remove(_loggedInUserFirebaseRef.key).then(function () {
+                log("user location removed for user: " + _loggedInUserFirebaseRef.key);
+                _deActivateDiv.hide();
+                _activateDiv.show();
+                _queryDiv.hide();
+            });
+        }
+    }
 
     function query() {
         logRef.empty();
@@ -29,10 +129,8 @@
         var start = Date.now();
         var radius = parseInt($('#distanceInput').val());
         log("*** Creating GeoQuery for radius " + radius + " ***");
-        // todo pass in the center from the UI
-        // todo on and off button that adds and removes to the locations
         var geoQuery = geoFire.query({
-            center: [51.5285, 0.0847],
+            center: _loggedInUserLocationPin,
             radius: radius
         });
 
@@ -41,12 +139,18 @@
             results.push({"key": key, "location": location, "distance": distance})
         });
 
+        var onKeyExitedRegistration = geoQuery.on("key_exited", function (key, location, distance) {
+            log(key + " exited the query. Distance is " + distance + " km");
+        });
+
         var onReadyRegistration = geoQuery.on("ready", function () {
             var duration = Date.now() - start;
-            log("*** Location data retrieved in " + duration + "ms ***");
-            log("*** 'ready' event fired - cancelling query ***");
-            geoQuery.cancel();
+            log("*** " + results.length + " Location records retrieved in " + duration + "ms ***");
+//            log("*** 'ready' event fired - cancelling query ***");
+//            geoQuery.cancel();
 
+            // the results appears to be received in the order in which they were entered into the db,
+            // rather than ordered from near to far
             results.sort(function(a, b) {
                 if (a.distance === b.distance) {
                     return 0;
@@ -71,14 +175,14 @@
                     return userSnapshot.val();
                 });
 
+                var durationUserLoad = Date.now() - startUserLoad;
+                log("*** " + users.length + " user records retrieved in " + durationUserLoad + "ms ***");
+
                 users.forEach(function(user, index) {
                     var location = results[index];
                     log("User: " + user.id + " distance: " + location.distance + " km: " + user.name
                         + " (" + user.locationName + ")");
                 });
-
-                var durationUserLoad = Date.now() - startUserLoad;
-                log("*** User data retrieved in " + durationUserLoad + "ms ***");
 
             }).catch(function(reason){
                 // if any of the promises fails.
@@ -87,45 +191,71 @@
         })
     }
 
+    function saveUser(email, name, photoURL) {
+        var keyFromEmail = replaceAll(email, '.', '');
+        var usersRef = firebaseDatabase.ref("users/" + keyFromEmail);
+        var userToSet = {
+            "id": keyFromEmail,
+            "email": email,
+            "name": name,
+            "photoURL": photoURL
+        };
+        usersRef.set(userToSet);
+        log("user created/updated: " + email + " with key: " + usersRef.key);
+        return usersRef;
+    }
+
     function addData() {
-        var users = [
-            {
-                "id": 1,
-                "name": "Leanne",
-                "locationPin": [51.5201, 0.0933],
-                "locationName": "Barbican"
-            },
-            {
-                "id": 2,
-                "name": "Jim",
-                "locationPin": [51.5797, 0.1237],
-                "locationName": "Crouch End"
-            },
-            {
-                "id": 3,
-                "name": "Su",
-                "locationPin": [51.5783, 0.2527],
-                "locationName": "West Hendon"
-            },
-            {
-                "id": 4,
-                "name": "Richard",
-                "locationPin": [51.9492, 0.2834],
-                "locationName": "Hitchin"
-            },
-            {
-                "id": 5,
-                "name": "Lewis",
-                "locationPin": [51.9038, 0.1966],
-                "locationName": "Stevenage"
-            },
-            {
-                "id": 6,
-                "name": "Paul",
-                "locationPin": [51.5285, 0.0847],
-                "locationName": "Shoreditch"
-            }
-        ];
+
+        var numUsers = 1000;
+        var users = [];
+        for (var i = 0; i < numUsers; i++) {
+            users[i] = {
+                "id": 7 + i,
+                "name": "User " + i,
+                "locationPin": [51.5201 - i * 0.001, 0.0933 - i * 0.001],
+                "locationName": "Location of User " + i
+            };
+        }
+
+//        var users = [
+//            {
+//                "id": 1,
+//                "name": "Leanne",
+//                "locationPin": [51.5201, 0.0933],
+//                "locationName": "Barbican"
+//            },
+//            {
+//                "id": 2,
+//                "name": "Jim",
+//                "locationPin": [51.5797, 0.1237],
+//                "locationName": "Crouch End"
+//            },
+//            {
+//                "id": 3,
+//                "name": "Su",
+//                "locationPin": [51.5783, 0.2527],
+//                "locationName": "West Hendon"
+//            },
+//            {
+//                "id": 4,
+//                "name": "Richard",
+//                "locationPin": [51.9492, 0.2834],
+//                "locationName": "Hitchin"
+//            },
+//            {
+//                "id": 5,
+//                "name": "Lewis",
+//                "locationPin": [51.9038, 0.1966],
+//                "locationName": "Stevenage"
+//            },
+//            {
+//                "id": 6,
+//                "name": "Paul",
+//                "locationPin": [51.5285, 0.0847],
+//                "locationName": "Shoreditch"
+//            }
+//        ];
 
         // Set the initial users and locations
         log("*** Setting initial users and locations ***");
@@ -140,11 +270,6 @@
                 log("user location for user: " + user.id + " set");
             });
         });
-
-//        RSVP.allSettled(userPromises)
-//            .then(function() {
-//
-//            });
     }
 
 
@@ -157,5 +282,14 @@
         var textNode = document.createTextNode(Date.now() + " : " + message);
         childDiv.appendChild(textNode);
         document.getElementById("log").appendChild(childDiv);
+    }
+
+    // http://stackoverflow.com/questions/1144783/how-to-replace-all-occurrences-of-a-string-in-javascript
+    function replaceAll(str, find, replace) {
+        return str.replace(new RegExp(escapeRegExp(find), 'g'), replace);
+    }
+
+    function escapeRegExp(str) {
+        return str.replace(/([.*+?^=!:${}()|\[\]\/\\])/g, "\\$1");
     }
 })();
